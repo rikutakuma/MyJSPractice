830ae2203ba5190eca23e4df0fb6c3b4a1c52573
榎本拓真 <t3460515@gmail.com>
2021-01-19 18:17:14 +0900

    javascript

---
+++ JAVASCRIPTBASICS/main.js
@@ -0,0 +1,60 @@
+'use strict';
+
+// console.log('hello');
+// console.log("hello");
+
+// console.log('it's me!);
+// console.log("it's me!");
+// console.log('it\'s me!');
+
+// console.log('he\nllo wor\tld!');
+
+// console.log('hello' + 'world');
+
+// console.log(10 + 3); 
+// console.log(10 - 3);
+// console.log(10 * 3);
+// console.log(10 / 3);
+// console.log(10 % 3);
+// console.log(10 ** 3);
+
+// 定数 const
+// 変数 let var
+
+// console.log(150 * 140);
+// console.log(150 * 160);
+
+// let price = 150;
+
+// console.log(price * 140);
+// console.log(price * 160);
+
+// const price = 170;
+
+// console.log(price * 140);
+// console.log(price * 160);
+
+let price = 500;
+
+//let price = price + 100;
+
+price += 100;
+
+//price = price * 2;
+price *= 2;
+
+// price = proce + 1;
+// price += 1;
+price++;
+
+// price -= 1;
+price--;
+
+console.log(price);
+
+console.log(typeof 5);
+
+
+
+
+
