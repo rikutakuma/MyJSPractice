db03afb0d17707fbbac6637f09c7f9883a74e522
榎本拓真 <t3460515@gmail.com>
2021-02-04 18:39:23 +0900

    javascript

---
+++ JAVASCRIPTBASICS2/main.js
@@ -0,0 +1,43 @@
+'use strict';
+
+{
+	// const score1 = 80;
+	// const score2 = 90;
+	// const score3 = 40;
+
+	const scores = [80, 90, 40, 70];
+	// console.log(scores[1]);
+
+	// scores[2] = 44;
+	// console.log(scores);
+
+	// console.log(scores.length);
+
+	// for (let i = 0; i < 3; i++) {
+		for (let i = 0; i < scores.length; i++) {
+		console.log(`Score ${i}: ${scores[i]}`);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
