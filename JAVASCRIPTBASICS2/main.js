c431d82f2155a3a56f305be24c2a9f8e9bd13f38
榎本拓真 <t3460515@gmail.com>
2021-02-05 23:22:22 +0900

    javascript

--- JAVASCRIPTBASICS2/main.js
+++ JAVASCRIPTBASICS2/main.js
@@ -5,7 +5,19 @@
 	// const score2 = 90;
 	// const score3 = 40;
 
-	const scores = [80, 90, 40, 70];
+	const otherScores = [10, 20];
+	const scores = [80, 90, 40, 70, ...otherScores];
+
+	function sum(a, b) {
+		console.log(a + b);
+	}
+	
+	sum(...otherScores);
+	// scores.push(60, 50);
+	// scores.shift();
+	//90, 40, 70, 60, 50
+
+	//scores.splice(1, 1, 40, 50);
 	// console.log(scores[1]);
 
 	// scores[2] = 44;
